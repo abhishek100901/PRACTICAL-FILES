8cb3548ed9e81f2f5156ddd03be578ac646db5e9
Abhishek Kumar Sharma <118673137+abhishek100901@users.noreply.github.com>
2023-01-24 09:07:34 +0530

    Add files via upload

---
+++ practicalone.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b) 
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int arr[], int n) 
+{
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+int binarySearch(int arr[], int l, int r, int x) 
+{
+    if (r >= l) {
+        int mid = (l + r) / 2;
+        if (arr[mid] == x) {
+            return mid;
+        }
+        if (arr[mid] > x) {
+            return binarySearch(arr, l, mid-1, x);
+        }
+        return binarySearch(arr, mid+1, r, x);
+    }
+    return -1;
+}
+
+int main() 
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x = 22;
+    bubbleSort(arr, n);
+    int result = binarySearch(arr, 0, n-1, x);
+    (result == -1) ? printf("Element is not present in array") : printf("Element is present at index %d", result);
+    return 0;
+}
